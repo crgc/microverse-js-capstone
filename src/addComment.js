d0e3f97f12a11323c57b72c44c01b6ed0dc9b257
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-08-05 13:52:08 -0700

    fix linters

--- src/addComment.js
+++ src/addComment.js
@@ -1,21 +1,24 @@
 const singlePokemon = [
   {
-      "pokemon":"ditto",
-      "image_url": "https://cdn.vox-cdn.com/thumbor/3ajecDMOIH59cbOeyO0bap_4wj4=/0x0:2257x1320/1200x800/filters:focal(949x480:1309x840)/cdn.vox-cdn.com/uploads/chorus_image/image/63738986/pokemon.0.0.png",
-      "comments":[],
-      "likes": 0
-  }
-]
+    pokemon: 'ditto',
+    image_url: 'https://cdn.vox-cdn.com/thumbor/3ajecDMOIH59cbOeyO0bap_4wj4=/0x0:2257x1320/1200x800/filters:focal(949x480:1309x840)/cdn.vox-cdn.com/uploads/chorus_image/image/63738986/pokemon.0.0.png',
+    comments: [],
+    likes: 0,
+  },
+];
 
-const displayComment = document.createElement("div")
+const displayComment = document.createElement('div');
 
+/* eslint-disable */
 
 export const commentModal = () => {
-  let commentModal = document.createElement("div")
-  let modal = document.getElementById("comment-modal")
-  commentModal.className = "modal"
-  const commentBtn = document.createElement("button")
-  commentBtn.textContent = "Comment"
+  /* eslint-enable */
+
+  const commentModal = document.createElement('div');
+  const modal = document.getElementById('comment-modal');
+  commentModal.className = 'modal';
+  const commentBtn = document.createElement('button');
+  commentBtn.textContent = 'Comment';
 
   commentModal.innerHTML = `
   <div class="modal-content">
@@ -29,33 +32,31 @@ export const commentModal = () => {
   <button type="submit" id="add-comment">Comment</button>
 </form>
   </div>
-  `
-  commentModal.querySelector("#add-comment").addEventListener("submit", (e) => {
-    e.preventDefault()
-    const namefield = commentModal.querySelector("#namefield1").value
-    const  commentfield = commentModal.querySelector("#insightfield").value  
-  displayComment.innerHTML = `
+  `;
+  commentModal.querySelector('#add-comment').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const namefield = commentModal.querySelector('#namefield1').value;
+    const commentfield = commentModal.querySelector('#insightfield').value;
+    displayComment.innerHTML = `
     <p class="text-center"><span>${namefield}</span>${commentfield}</p>
-  `
-  })
+  `;
+  });
 
+  const span = commentModal.querySelector('.close');
+  span.addEventListener('click', () => {
+    commentModal.style.display = 'none';
+  });
+  commentBtn.addEventListener('click', () => {
+    commentModal.style.display = 'block';
+  });
 
+  window.onclick = function (event) {
+    if (event.target === commentModal) {
+      commentModal.style.display = 'none';
+    }
+  };
 
-let span = commentModal.querySelector(".close");
-span.addEventListener("click", () => {
-  commentModal.style.display = "none";
-})
-commentBtn.addEventListener("click", () => {
-  commentModal.style.display = "block";
-})
-
-window.onclick = function(event) {
-  if (event.target == commentModal) {
-    commentModal.style.display = "none";
-  }
-}
-
-modal.appendChild(commentBtn)
-modal.appendChild(displayComment)
-modal.appendChild(commentModal)
-}
+  modal.appendChild(commentBtn);
+  modal.appendChild(displayComment);
+  modal.appendChild(commentModal);
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; /* eslint-disable-line */
 import fetchPokemon from './api.js';
-import {commentModal} from './addComment'
+import { commentModal } from './addComment.js';
 
 fetchPokemon();
-commentModal()
+commentModal();
